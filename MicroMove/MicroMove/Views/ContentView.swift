c54c7d8396db57c8e564bb35a80f4e1516b8c25c
DaniiB26 <daniib1426@gmail.com>
2025-06-24 12:59:04 +0300

    Content View Updated for the New Functionalities

--- MicroMove/MicroMove/Views/ContentView.swift
+++ MicroMove/MicroMove/Views/ContentView.swift
@@ -4,12 +4,42 @@ import SwiftData
 /// The main entry point for the app, displaying the exercise library.
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
+    @State private var showAchievements = false
 
     var body: some View {
         NavigationStack {
             // Show the exercise list with filtering and sorting
             ExerciseListView(viewModel: ExercisesViewModel(modelContext: modelContext))
+
+            NavigationLink(
+                destination: AchievementListView(
+                    viewModel: AchievementsViewModel(modelContext: modelContext)
+                ),
+                isActive: $showAchievements
+            ) {
+                EmptyView()
+            }
+
             .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    NavigationLink(
+                        destination: ProgressView(viewModel: ProgressViewModel(modelContext: modelContext)) 
+                    )
+                    {
+                        Image(systemName: "person")
+                    }
+                    .accessibilityLabel("Progress")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        Button("Achievements") {
+                            showAchievements = true
+                        }
+                        Button("Activity Log", action: {print("Activity Log Selected")}) 
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                    }
+                }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink(
                         destination: UserPreferencesView(
