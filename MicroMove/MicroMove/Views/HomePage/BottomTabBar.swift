2bcc656c8b237e3d935fff62d765d52cdec86cf7
DaniiB26 <daniib1426@gmail.com>
2025-08-06 14:27:33 +0300

    Bottom Tab Bar

---
+++ MicroMove/MicroMove/Views/HomePage/BottomTabBar.swift
@@ -0,0 +1,61 @@
+import SwiftUI
+import SwiftData
+
+struct BottomTabBar: View {
+    let modelContext: ModelContext
+    @ObservedObject var progressViewModel: ProgressViewModel
+    @ObservedObject var userPreferencesViewModel: UserPreferencesViewModel
+    @ObservedObject var activityLogViewModel: ActivityLogViewModel
+    @ObservedObject var workoutSessionViewModel: WorkoutSessionViewModel
+    var activityMonitor: ActivityMonitor?
+
+    var body: some View {
+        TabView {
+            // Home tab
+            NavigationStack {
+                    ZStack {
+                        Color(.systemGray6)
+                            .ignoresSafeArea()
+
+                        VStack(spacing: 16) {
+                            HomeView(progressViewModel: progressViewModel, userPreferencesViewModel: userPreferencesViewModel)
+                        }
+                        .padding(.horizontal)
+                        .padding(.top, 12)
+                }
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .tabItem {
+                Label("Home", systemImage: "house")
+            }
+
+            // Workouts tab
+            NavigationStack {
+                // Text("Workouts View (To be implemented)")
+                //     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                //     .background(Color(.systemGray6))
+                ExerciseListView(
+                    exerciseViewModel: ExercisesViewModel(modelContext: modelContext),
+                    activityLogViewModel: activityLogViewModel,
+                    workoutSessionViewModel: workoutSessionViewModel,
+                    progressViewModel: progressViewModel,
+                    activityMonitor: activityMonitor
+                )
+            }
+            .tabItem {
+                Label("Workouts", systemImage: "dumbbell")
+            }
+
+            // Achievements tab
+            NavigationStack {
+                Text("Achievements View (To be implemented)")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color(.systemGray6))
+            }
+            .tabItem {
+                Label("Achievements", systemImage: "medal")
+            }
+        }
+        .accentColor(.black)
+    }
+}
